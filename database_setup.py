41362452739ad3c0207aa870c90fda6cc80e2cfb
Kevin Neag <neagkv@gmail.com>
2018-11-08 21:20:57 -0500

    refactor: update imports and add columns to the item table

--- database_setup.py
+++ database_setup.py
@@ -1,16 +1,28 @@
+import sys
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Items(Base):
-    __tablename__ = 'items'
+class Category(Base):
+  __tablename__ = "Category"
+
+
+
+class Item(Base):
+    __tablename__ = 'item'
 
 
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
+    price = Column(String(8))
+    category_id = Column(Integer, ForeignKey('category_id.id'))
+    category = relationship(Category)
+
 
     @property
     def serialize(self):
