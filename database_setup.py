29af25eccaf887408d6fb433367616f443e56e92
Kevin Neag <neagkv@gmail.com>
2018-11-11 10:06:30 -0500

    refactor: database_setup

--- database_setup.py
+++ database_setup.py
@@ -1,42 +1,50 @@
+import os
 import sys
-
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class Category(Base):
-  __tablename__ = "Category"
+    __tablename__ = 'category'
 
-  name =Column(String(80), nullable = False)
-  id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 
 class Item(Base):
     __tablename__ = 'item'
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
-    category_id = Column(Integer, ForeignKey('category_id.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-          'id': self.id,
-           'name': self.name,
-           'description' : self.description
-       }
-
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+        }
 
 
 engine = create_engine('sqlite:///items.db')
+
+
 Base.metadata.create_all(engine)
